e7e8bfbe5050117f132df2d5835e015f030d3933
anneau <hkobori@gmx.com>
2022-04-10 23:14:07 +0900

    chore(core): install tailwind to main package.json
    install tailwind to main package.json and remove tailwind from portfolio

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "../../packages/ui/components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
