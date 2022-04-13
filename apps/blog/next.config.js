6ce47d02221fa18f5cfc55a6b6ac5e64fbb0b74b
kobori.hikaru <kobori.hikaru@a-hikkoshi.co.jp>
2022-04-13 19:04:25 +0900

    chore(blog): install relay
    install relay and relay-compiler, and build settings
    
    Closes: #13

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -2,4 +2,13 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
+  pageExtensions: ["tsx"],
+  compiler: {
+    relay: {
+      // This should match relay.config.js
+      src: "./",
+      artifactDirectory: "__generated__",
+      language: "typescript",
+    },
+  },
 });
